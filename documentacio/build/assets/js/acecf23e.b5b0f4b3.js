5fce19fe35daefd6b25e33f67d7413a0e4d70c08
xxjcaxx <jocasal@gmail.com>
2025-01-20 08:27:54 +0100

    build

---
+++ documentacio/build/assets/js/acecf23e.b5b0f4b3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocumentacio=self.webpackChunkdocumentacio||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
